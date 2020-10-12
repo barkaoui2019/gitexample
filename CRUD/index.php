ae03b866aa14d73fb7bae63ada17ca46638be614
barkaoui2019 <cheker.barkaoui@supcom.tn>
2020-10-12 21:27:14 +0100

    My first commit on this project

---
+++ CRUD/index.php
@@ -0,0 +1,52 @@
+<?php
+require_once("../CRUD/php/component.php")
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport"
+content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible"content="ir=edge">
+<title> DHIA </title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<!--Custom stylsheet-->
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+<main>
+  <div class="container text-center">
+    <h1 class="py-4 bg-dark text-light rounded"><i class="fas fa-swatchbook"></i>Book Store</h1>
+    <div class="d-flex justify-content-center">
+     <form action="" method="post" class="w-50">
+       <div class="py-2">
+          <div class="input-group mb-2">
+             <div class="input-group-prepend">
+              <div class="input-group-text bg-warning"><i class="fas fa-id-badge"></i></div>
+             </div>
+             <input type="text" autocomplete="off" placeholder="ID" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+           </div>
+       </div>
+       <div class="pt-2">
+       <?php inputElement();?>
+       
+           
+       
+         
+      </div>
+     </form>
+    </div>
+  </div>
+  </main>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+</body>
+</html>
