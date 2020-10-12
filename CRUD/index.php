1662e340f76738e3a0c228e0f702bfc67e2a5317
barkaoui2019 <cheker.barkaoui@supcom.tn>
2020-10-12 21:51:38 +0100

    A title has been changed

--- CRUD/index.php
+++ CRUD/index.php
@@ -10,7 +10,7 @@ require_once("../CRUD/php/component.php")
 <meta name="viewport"
 content="width=device-width,user-scalable=no,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0">
 <meta http-equiv="X-UA-Compatible"content="ir=edge">
-<title> DHIA </title>
+<title> Chaker </title>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <!--Custom stylsheet-->
